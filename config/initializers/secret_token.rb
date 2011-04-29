a5cd9fe13b626e8bebe0fc75e18d04fa3b0ae0d8
U-SHPD\weith <weith@Weith.shpd.fastrackids.com.cn>
2011-04-29 19:26:17 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e8cebb3e10d43396892b473cd60d356336aaf3a8a7ca209496b5b1a482f3481b6e1e5902785e8f79e0b9c160c3307d417d25209a0a49f4b52b6832246a569322'
